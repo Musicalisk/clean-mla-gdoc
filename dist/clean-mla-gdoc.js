131662995deee23c696c043f02f1a1fde5efb9c0
Musicalisk <musicalisk.travail@tuta.io>
2026-03-04 11:17:15 -0500

    Bumped to version 1.1.2

--- dist/clean-mla-gdoc.js
+++ dist/clean-mla-gdoc.js
@@ -1,5 +1,5 @@
 /*
- * clean-mla-gdoc.js v1.1.1
+ * clean-mla-gdoc.js v1.1.2
  * Copyright (c) 2026 Musicalisk <Musicalisk.travail@tuta.io>
  * Licensed under the GNU General Public License v3.0
  * Full license text: https://www.gnu.org
